b02f6ccd29fbd535510cacb112821b408ed7de3b
zakareeya <Szakareeya@ptnicec.ac.th>
2018-11-08 14:49:40 +0700

    send project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,44 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController,
+    public navCtrl: NavController) {
 
   }
-
+  showAlert1(){
+    const alert = this.alertCtrl.create({
+      title: 'แจ้งเตือนการใช้งาน',
+      subTitle: 'ของระบบ',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  showAlert2(){
+    const alert = this.alertCtrl.create({
+      title: 'โปรดใส่รหัสผ่านที่สมัครไว้',
+      message: 'TEST MESSAGE',
+      subTitle: 'จำนวน 6 ตัวขึ้นไป',
+      inputs: [
+        {
+          name: 'Login',
+          placeholder: '*******'
+        }
+      ],
+      buttons: [
+        {
+          text: 'ยกเลิก',
+          handler: () => {
+            console.log('ยกเลิกแล้ว');
+          }
+        },
+        {
+          text: 'บันทึก',
+          handler: () => {
+            console.log('บันทึกแล้ว');
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
 }
